5cb09c942d9c47038344de490454184795f70eac
Soft illusion <softillusion.robotics@gmail.com>
2021-08-29 01:14:46 -0500

    home screen improved

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: Color(0xFF3EBACE),
         accentColor: Color(0xFFD8ECF1),
-        scaffoldBackgroundColor: Color(0xFFF3F5F7),
+        scaffoldBackgroundColor: Color(0xFFF1EABA),
       ),
       home: Homescreen(),
     );
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -21,11 +21,13 @@ class _HomescreenState extends State<Homescreen> {
         child: ListView(
           children: <Widget>[
             Padding(
-              padding: EdgeInsets.only(left: 20.0, right: 120.0, top: 30.0),
-              child: Text('Choose a sensor',
+              padding: EdgeInsets.only(left: 20.0, right: 20.0, top: 30.0),
+              child: Text('Use phone a sensor on your robot.',
                 style: TextStyle(
                   fontSize: 30.0,
                   fontWeight: FontWeight.normal,
+                  color: Colors.green.withOpacity(0.7),
+
                 ),
               ),
             ),
@@ -34,82 +36,53 @@ class _HomescreenState extends State<Homescreen> {
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
                 Padding(
-                    padding: EdgeInsets.only(top: 30.0),
-                    child: ElevatedButton(
-                      onPressed: () {
-                        print("Camera clicked");
-                      },
-                      child: Container(
-                        height: 60.0,
-                        width: 150.0,
-                        decoration: BoxDecoration(
-                          color: Color(0xFFA8D7C8),
-                          borderRadius: BorderRadius.circular(5.0),
-                          border: Border.all(color: Colors.black),
-                        ),
-                        child: Text(
-                          'Camera',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            color: Color(0xE81616FF),
-                            fontSize: 20.0,
-                          ),
-
-                        ),
-                      ),
-                    )
-
-                ),
-
-                Padding(
                   padding: EdgeInsets.only(top: 30.0),
                   child: ElevatedButton(
                     onPressed: () {
                       print("IMU clicked");
                     },
-                    child: Container(
-                      height: 60.0,
-                      width: 150.0,
-                      decoration: BoxDecoration(
-                        color: Color(0xFFA8D7C8),
-                        borderRadius: BorderRadius.circular(5.0),
-                        border: Border.all(color: Colors.black),
-                      ),
                       child: Text(
                         'IMU',
                         textAlign: TextAlign.center,
                         style: TextStyle(
-                          color: Color(0xE81616FF),
+                          color: Colors.white.withOpacity(0.7),
                           fontSize: 20.0,
                         ),
                       ),
                     ),
                   ),
-                ),
+                Padding(
+                    padding: EdgeInsets.only(top: 30.0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        print("Camera clicked");
+                      },
+                      child: Text(
+                        'Camera',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white.withOpacity(0.7),
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    )
 
+                ),
                 Padding(
                   padding: EdgeInsets.only(top: 30.0),
                   child: ElevatedButton(
                     onPressed: () {
                       print("Stream data clicked");
                     },
-                    child: Container(
-                      height: 60.0,
-                      width: 150.0,
-                      decoration: BoxDecoration(
-                        color: Color(0xFFA8D7C8),
-                        borderRadius: BorderRadius.circular(5.0),
-                        border: Border.all(color: Colors.black),
-                      ),
                       child: Text(
-                        'Stream Data',
+                        'Stream Data to ROS',
                         textAlign: TextAlign.center,
                         style: TextStyle(
-                          color: Color(0xE81616FF),
+                          color: Colors.white.withOpacity(0.7),
                           fontSize: 20.0,
                         ),
                       ),
-                    ),
+
                   ),
                 ),
               ],
