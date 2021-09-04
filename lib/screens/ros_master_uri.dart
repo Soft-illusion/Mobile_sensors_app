8065109d9402673c1c455e0158e49fba4dc45663
Soft illusion <softillusion.robotics@gmail.com>
2021-09-03 19:41:08 -0500

    Made dynamic fields to accept IP and port number

--- lib/screens/ros_master_uri.dart
+++ lib/screens/ros_master_uri.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:dartx/dartx.dart';
 import 'package:std_msgs/msgs.dart';
 import 'package:sensor_msgs/msgs.dart';
- import 'dart:io';
+import 'dart:io';
 
 class RosStreamer extends StatefulWidget {
   const RosStreamer({Key? key}) : super(key: key);
@@ -15,15 +15,18 @@ class RosStreamer extends StatefulWidget {
 class _RosStreamerState extends State<RosStreamer> {
   late NodeHandle nh;
   late Publisher imu_pub;
+  String IpAddressRos = '192.168.0.100';
+  String RosPort = '11311'; 
+  bool connected = false;
 
   @override
   void initState() {
     super.initState();
-    init_ros();
+    
   }
 
   void init_ros() async {
-    nh = await initNode("Hey", [], rosMasterUri: "http://192.168.0.107:11311");
+    nh = await initNode("Hey", [], rosMasterUri: 'http://'+IpAddressRos+':'+RosPort);
     imu_pub = nh.advertise<StringMessage>("/imu/raw", StringMessage.$prototype);
   }
 
@@ -33,22 +36,96 @@ class _RosStreamerState extends State<RosStreamer> {
       appBar: AppBar(
         title: Text("ROS Streamer"),
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () async{
-            final imu_msg = StringMessage(data: "Hi!!");
-            for (int i=0;i<10;i++){
-              print("Publish button pressed");
-              imu_pub.publish(imu_msg,1);
-              await Future.delayed(2.seconds);
-            }
-            // sleep(const Duration(data: RosTime(secs: 1)));
-          },
-          child: Text(
-            "Press to Publish",
-            textAlign: TextAlign.center,
-          ),
-        ),
+ 
+      body: SafeArea(
+        child: ListView(
+          children: <Widget>[
+            SizedBox(height: 20.0),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(top: 30.0),
+                  child:TextFormField(
+                    initialValue: IpAddressRos,
+                    onChanged: (String value){
+                      IpAddressRos = value;
+                    },
+                    obscureText: false,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Roscore IP',
+                    ),
+                  )
+                ),
+                
+                Padding(
+                  padding: EdgeInsets.only(top: 30.0),
+                  child:TextFormField(
+                      initialValue: RosPort,
+                      onChanged: (String port){
+                        RosPort = port;
+                    },
+                    obscureText: false,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Port Number',
+                    ),
+                  )
+                ),
+                
+                Padding(
+                  padding: EdgeInsets.only(top: 30.0),
+                  child: ElevatedButton(
+                     style: ElevatedButton.styleFrom(
+                          onPrimary: Colors.black,
+                          primary: Colors.red,
+                          ) , 
+                    onPressed: () async{
+                      try {
+                        print('Port :' + RosPort);
+                        print('IP :' + IpAddressRos);
+
+                        connected = true;
+                        // try catch not catching the non roscore IP
+                        init_ros();
+                      }
+                      on Exception catch (_){
+                        connected = false;
+                      }
+                    },
+                    child: Text(
+                      "Connect",
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 30.0),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                          onPrimary: Colors.black,
+                          primary: connected? Colors.green:Colors.red, // not functional as this gets color when it loads. Need dynamic reconfiguration.
+                          ) , 
+                    onPressed: () async{
+                      print(connected);
+                      final imu_msg = StringMessage(data: "Hi!!");
+                      for (int i=0;i<10;i++){
+                        print("Publish button pressed");
+                        imu_pub.publish(imu_msg,1);
+                        await Future.delayed(2.seconds);
+                      }
+                    },
+                    child: Text(
+                      "Press to Publish",
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+            ],
+            ),
+          ],
+       ),
       ),
     );
   }
